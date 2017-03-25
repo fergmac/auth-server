a009a85e0bf99c82d2b22ff7749f018e6221f241
fergmac <f.macconnell@gmail.com>
2017-03-24 19:48:55 -0700

    define model

--- models/user.js
+++ models/user.js
@@ -1,1 +1,13 @@
 // local definition of what a user is
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema; // we use to tell mongoose about particular fields model should have
+
+// Define model
+const userSchema = new Schema({
+    email: { type: String, unique: true, lowercase: true },
+    password: String
+});
+
+// Create model class
+
+// Export the model
