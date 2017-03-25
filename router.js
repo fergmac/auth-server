f8c4e205ae23218c4aec64e3a37df2e81afe169c
fergmac <f.macconnell@gmail.com>
2017-03-24 22:42:43 -0700

    create signup route

--- router.js
+++ router.js
@@ -1,8 +1,17 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+
+const requireAuth = passport.authenticate('jwt', { session: false });
+
 
 // export function, import into index.js and pass app into function
 //route handler
 module.exports = function(app) {
     //define route
+    app.get('/', requireAuth, function(req, res) {
+        res.send({ hi: 'there' });
+    });
     app.post('/signup', Authentication.signup);
 }
